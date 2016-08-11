e4125a93ef658ffc7841b41217be46093211e616
Ankit Gupta <ankit.gupta8898@gmail.com>
2016-08-10 19:01:24 -0700

    removed some dead loggers

--- lib/optimizely_config_provider/helpers/support.rb
+++ lib/optimizely_config_provider/helpers/support.rb
@@ -21,15 +21,14 @@ module OptimizelyConfigProvider
     # end
 
     def experiment(experiment_key, &blk)
-      result_variation_key = optimizely_sdk_project_instance(experiment_key)#OptimizelyConfigProvider::OptimizelySdk.project_instance(event_dispather: MyEventDispatcher.new).activate(experiment_key,user_id.sample)
+      result_variation_key = optimizely_sdk_project_instance(experiment_key)
       variation_instance   = OptimizelyConfigProvider::Variation.new(result_variation_key)
       blk.call(variation_instance)
       variation_instance.compute
     end
 
     def optimizely_sdk_project_instance(experiment_key)
-      user_id = ['12345','123456','12345678']
-      OptimizelyConfigProvider::OptimizelySdk.project_instance(event_dispather: MyEventDispatcher.new).activate(experiment_key, user_id.sample)
+      OptimizelyConfigProvider::OptimizelySdk.project_instance(event_dispather: MyEventDispatcher.new).activate(experiment_key, OptimizelyConfigProvider.configuration.visitor_id)
     end
 
   end
