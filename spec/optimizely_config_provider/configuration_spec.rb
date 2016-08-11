67fb2546e04deb0c8cdde1db688f8c1bd0b50ffc
Ankit Gupta <ankit.gupta8898@gmail.com>
2016-08-10 18:44:22 -0700

    Some more specs around config

--- spec/optimizely_config_provider/configuration_spec.rb
+++ spec/optimizely_config_provider/configuration_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 RSpec.describe OptimizelyConfigProvider::Configuration do
 
+  subject { OptimizelyConfigProvider::Configuration.new }
 
   describe '#config_endpoint' do
-
-    subject { OptimizelyConfigProvider::Configuration.new }
 
     it 'has a default value of http://foo.com' do
       expect(subject.config_endpoint).to eq('http://foo.com')
     end
 
+  end
+
+  describe '#visitor_id' do
+
+    it 'defaults to nil' do
+      expect(subject.visitor_id).to be_nil
+    end
   end
 end
--- spec/optimizely_config_provider_spec.rb
+++ spec/optimizely_config_provider_spec.rb
@@ -3,12 +3,55 @@ require 'spec_helper'
 RSpec.describe OptimizelyConfigProvider do
 
   describe "#configure" do
-    before do
-      OptimizelyConfigProvider.configure do |config|
-        config.config_endpoint = 'https://cdn.optimizely.com/json/5960232316.json'
+
+    context 'when config is set in regular way' do
+
+      before do
+        OptimizelyConfigProvider.configure do |config|
+          config.config_endpoint = 'https://cdn.optimizely.com/json/5960232316.json'
+          config.cache_expiry    = 12
+        end
+      end
+
+      it 'has config_endpoint' do
+        expect(OptimizelyConfigProvider.configuration.config_endpoint).to eq('https://cdn.optimizely.com/json/5960232316.json')
+      end
+
+      it 'has cache_expiry' do
+        expect(OptimizelyConfigProvider.configuration.cache_expiry).to eq(12)
+      end
+
+      it 'has no visitor_id' do
+        expect(OptimizelyConfigProvider.configuration.visitor_id).to be_nil
       end
     end
 
+
+    context 'when config is set in between' do
+
+      before do
+        OptimizelyConfigProvider.configure do |config|
+          config.config_endpoint = 'https://cdn.optimizely.com/json/5960232316.json'
+          config.cache_expiry    = 12
+        end
+      end
+
+      it 'has config_endpoint' do
+        OptimizelyConfigProvider.configure do |config|
+          config.visitor_id = '1234abcdef'
+        end
+
+        expect(OptimizelyConfigProvider.configuration.config_endpoint).to eq('https://cdn.optimizely.com/json/5960232316.json')
+      end
+
+      it 'has cache_expiry' do
+        expect(OptimizelyConfigProvider.configuration.cache_expiry).to eq(12)
+      end
+
+      it 'has no visitor_id' do
+        expect(OptimizelyConfigProvider.configuration.visitor_id).to eq('1234abcdef')
+      end
+    end
 
   end
 end
