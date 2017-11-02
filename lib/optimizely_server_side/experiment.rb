a48895b2c64fee85b26de23ab6d6c943cd17160d
Ankit Gupta <ankit.gupta8898@gmail.com>
2017-11-02 10:55:19 -0700

    Bad indentation fixed

--- lib/optimizely_server_side/experiment.rb
+++ lib/optimizely_server_side/experiment.rb
@@ -51,38 +51,38 @@ module OptimizelyServerSide
     # Selects and calls the variation which is applicable
     # In case of running test the applicable variation key is present
     # In case of fallback / paused test we pick the primary variation
-           def applicable_variation
-             ActiveSupport::Notifications.instrument "oss.variation", variation: @selected_variation_key, experiment: @experiment_key, visitor_id: OptimizelyServerSide.configuration.user_attributes['visitor_id'] do
-               if @variations.any?(&variation_selector)
-                 @variations.find(&variation_selector).call
-               else
-                 primary_variation.call if primary_variation
-               end
-             end
-           end
+    def applicable_variation
+      ActiveSupport::Notifications.instrument "oss.variation", variation: @selected_variation_key, experiment: @experiment_key, visitor_id: OptimizelyServerSide.configuration.user_attributes['visitor_id'] do
+        if @variations.any?(&variation_selector)
+          @variations.find(&variation_selector).call
+        else
+          primary_variation.call if primary_variation
+        end
+      end
+    end
 
-           # Primary variation is where primary: true
-           def primary_variation
-             @primary_variation ||= @variations.find(&:primary)
-           end
+    # Primary variation is where primary: true
+    def primary_variation
+      @primary_variation ||= @variations.find(&:primary)
+    end
 
-           private
+    private
 
-           # Scope to query on selected variation
-           def variation_selector
-             ->(variation) { variation.key == @selected_variation_key }
-           end
+    # Scope to query on selected variation
+    def variation_selector
+      ->(variation) { variation.key == @selected_variation_key }
+    end
 
-           # Add all the variation to the variations collection
-           def add_variation(key, opts = {}, &blk)
-             Variation.new(
-               key: key,
-               primary: opts[:primary] || false,
-               content: blk
-             ).tap do |variation_instance|
-               @variations << variation_instance
-             end
-           end
+    # Add all the variation to the variations collection
+    def add_variation(key, opts = {}, &blk)
+      Variation.new(
+        key: key,
+        primary: opts[:primary] || false,
+        content: blk
+      ).tap do |variation_instance|
+        @variations << variation_instance
+      end
+    end
 
-           end
-           end
+  end
+end
